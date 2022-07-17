d00dc8160faf5ea03bb2b4ccb30b04418d73a1b0
Pauolosavery <pauolosavery1@gmail.com>
2022-07-17 23:13:01 +0300

    ready app without CSS

--- src/App.js
+++ src/App.js
@@ -5,18 +5,39 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import axios from 'axios';
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 export default function BasicTextFields() {
   const [name, setName] = useState('');
   const [group, setGroup] = useState('');
   const [phase, setPhase] = useState('');
+  const [APIdata, setAPIdata] = useState([]);
+  const [refresh, setRefresh] = useState([]);
   
   const onSubmit = () => {
     axios.post(`https://sheet.best/api/sheets/13513c0d-24f7-4d21-87f3-7eb20ca397ea`, {
       name, group, phase
     })
+    .then((data) => {
+      setRefresh(data);
+    })
   }
 
+  useEffect(()=>{
+    axios.get(`https://sheet.best/api/sheets/13513c0d-24f7-4d21-87f3-7eb20ca397ea`)
+    .then((incomingData)=> {
+      setAPIdata(incomingData.data);
+    })
+  },[refresh])
+
   return (
+    <>
     <Box
       component="form"
       sx={{
@@ -36,5 +57,31 @@ export default function BasicTextFields() {
         Отправить
       </Button>
     </Box>
+    <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Имя студента</TableCell>
+                <TableCell align="right">Группа</TableCell>
+                <TableCell align="right">Фаза</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {APIdata.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.group}</TableCell>
+                  <TableCell align="right">{row.phase}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    </>
   );
 }
